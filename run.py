c2c1a35334cebf4cfe7cde55ddab502e353e4fdc
tomcollins2110 <tom206collins@gmail.com>
2023-07-06 23:13:07 +0000

    Added data for board and ships creation and location

--- run.py
+++ run.py
@@ -13,3 +13,39 @@ def print_board(board):
         print("%d|%s|" % (row_num, "|".join(row)))
         row_num +=1
 
+def get_ship_location():
+
+    row=input('Please enter a ship row 1-6 ').upper()
+    while row not in '123456':
+        print("Please enter a valid row ")
+        row=input('Please enter a ship row 1-6 ')
+
+    column=input('Please enter a ship column A-F ').upper()
+    while column not in 'ABCDEF':
+        print("Please enter a valid column ")
+        column=input('Please enter a ship column A-F ')
+    return int(row)-1,let_to_num[column]
+
+def create_ships(board):
+    for ship in range(5):
+        ship_r, ship_cl=randint(0,7), randint(0,7)
+        while board[ship_r][ship_cl] =='X':
+            ship_r, ship_cl = randint(0, 7), randint(0, 7)
+        board[ship_r][ship_cl] = 'X'
+
+
+def count_hit_ships(board):
+    count=0
+    for row in board:
+        for column in row:
+            if column=='X':
+                count+=1
+    return count
+
+create_ships(Hidden_Pattern)
+#print_board(Hidden_Pattern)
+turns = 10
+while turns > 0:
+    print('Welcome to Battleships!')
+    print_board(Guess_Pattern)
+    row,column =get_ship_location()
