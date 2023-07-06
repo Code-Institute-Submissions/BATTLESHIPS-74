1ea367da9225ffa63f32711f391cf71446309eaa
tomcollins2110 <tom206collins@gmail.com>
2023-07-06 21:21:50 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+Hidden_Pattern=[[' ']*6 for x in range(6)]
+Guess_Pattern=[[' ']*6 for x in range(6)]
+
+let_to_num={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5}
+
+def print_board(board):
+    print(' A B C D E F ')
+    print(' ***********')
+    row_num=1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num +=1
+
+print_board(Guess_Pattern)
